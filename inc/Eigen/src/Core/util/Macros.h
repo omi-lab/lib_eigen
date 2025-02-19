31d7d9ed6e7017e4e78dfbb275a548fb1678b21f
Tom Paynter <tompaynter@tdpe.co.uk>
2025-02-19 18:50:39 +0000

    Merged from mono.

--- inc/Eigen/src/Core/util/Macros.h
+++ inc/Eigen/src/Core/util/Macros.h
@@ -599,9 +599,10 @@
 // Does the compiler support result_of?
 // It's likely that MSVC 2013 supports result_of but I couldn't not find a good source for that,
 // so let's be conservative.
+//ABlinov NOTE: results_of deprecated and not suported anytmore from version C++17 new check is added reverting to conservative code
 #ifndef EIGEN_HAS_STD_RESULT_OF
 #if EIGEN_MAX_CPP_VER>=11 && \
-    (__has_feature(cxx_lambdas) || (defined(__cplusplus) && __cplusplus >= 201103L) || EIGEN_COMP_MSVC >= 1900)
+    (__has_feature(cxx_lambdas) || (defined(__cplusplus) && __cplusplus >= 201103L && __cplusplus < 201703L) || ( EIGEN_COMP_MSVC >= 1900 && __cplusplus < 201703L))
 #define EIGEN_HAS_STD_RESULT_OF 1
 #else
 #define EIGEN_HAS_STD_RESULT_OF 0
